8810c47831bc8d3e594bde1ab1e50e9097625f11
Adnan <adnan.nanan13@gmail.com>
2024-03-29 14:42:44 +0700

    Membuat proses login ke halaman dashboard

---
+++ dashboard.php
@@ -0,0 +1,7 @@
+<?php
+include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "spp_sma";
