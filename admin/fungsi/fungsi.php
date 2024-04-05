b228d2edd5d0bebb477e0df8eca8df61d475188c
Adnan <adnan.nanan13@gmail.com>
2024-04-05 10:36:15 +0700

    Update fungsi

--- admin/fungsi/fungsi.php
+++ admin/fungsi/fungsi.php
@@ -1,4 +1,21 @@
 <?php
 
 // koneksi
-$koneksi = mysqli_connect('localhost', 'root', '', 'spp_kp');
+$koneksi = mysqli_connect('localhost', 'root', '', 'spp_smadm');
+
+function summon_admin()
+{
+    global $koneksi;
+    $id = $_SESSION['idsppapp'];
+    return mysqli_query($koneksi, "SELECT * FROM admin WHERE id='$id'");
+}
+
+function gambar($img, $size)
+{
+    echo '<img src="//localhost/kerja-praktek/assets/images/' . $img . '" width="' . $size . '">';
+}
+
+function url()
+{
+    return $url = "//localhost/kerja-praktek/assets/";
+}
